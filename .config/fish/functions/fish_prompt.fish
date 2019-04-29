9f820559e9b60e5718acb1b126272a532715bd6f
Sergey <sergey.sh@legalbet.com>
2019-04-29 17:14:30 +0600

    fix bin

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -2,6 +2,6 @@ function fish_prompt
     export real_status=$status
     export COLUMNS=$COLUMNS
     # https://github.com/justjanne/powerline-go#fish
-    $HOME/.dofiles/bin/powerline-go -shell bare
-    #eval $HOME/.dofiles/bin/powerline-go -error $real_status -shell bare -eval -colorize-hostname -modules-right user
+    $HOME/bin/powerline-go -shell bare
+    #eval $HOME/bin/powerline-go -error $real_status -shell bare -eval -colorize-hostname -modules-right user
 end
