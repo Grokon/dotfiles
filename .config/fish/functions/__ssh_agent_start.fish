6a3ec498e225d8936c0580aba71033f93b055b16
Grokon <4414811+Grokon@users.noreply.github.com>
2022-05-27 14:07:31 +0300

    fix ssh-agent && add greeting

--- .config/fish/functions/__ssh_agent_start.fish
+++ .config/fish/functions/__ssh_agent_start.fish
@@ -1,6 +1,6 @@
 function __ssh_agent_start -d "start a new ssh agent"
-    set --local SSH_ENV $HOME/.ssh/agent.env
+    set --local SSH_ENV $HOME/.ssh/agent.env.fish
     source $SSH_ENV > /dev/null
-    ssh-agent -s | sed 's/^echo/#echo/' >| $SSH_ENV
+    ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
     chmod 600 $SSH_ENV
 end
--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -2,5 +2,27 @@ function fish_greeting
   if test -e /var/run/motd.dynamic -a "$SINGLE_COMMAND" != "true"
     set_color 4E9A06
     cat /var/run/motd.dynamic
+  else
+    
+    if test -d /run/WSL
+      set -f line1 (uname --kernel-name)" - WSL 2"
+    else 
+      set -f line1 '(uname --kernel-name)'
+    end
+    echo "                 ___"
+    echo "   ___======____=---=)                  "
+    echo " /T            \_--===)                 kernel-name:        " $line1
+    echo " [ \ (0)   \~    \_-==)                 nodename:           " (uname --nodename)
+    echo "  \      / )J~~    \-=)                 kernel-release:     " (uname --kernel-release)
+    echo "   \\___/  )JJ~~~   \)                   kernel-version:     " (uname --kernel-version)
+    echo "    \_____/JJJ~~~~    \                 machine:            " (uname --machine)
+    echo "    / \  , \J~~~~~     \                processor:          " (uname --processor)
+    echo "   (-\)\=|\\\~~~~       L__              hardware-platform:  " (uname --hardware-platform)
+    echo "   (\\)  (\\\)_           \==__"
+    echo "    \V    \\\) ===_____   \\\\\\"
+    echo "           \V)     \_) \\\\JJ\J\)"
+    echo "                       /J\JT\JJJJ"
+    echo "                       (JJJ| \UUU"
+    echo "                        (UU)"
   end
 end
